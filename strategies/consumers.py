ab1ca4dddf1dd7f5c386ae7b12ad34a9aca7e09b
zaebee <zaebuntu@gmail.com>
2018-04-05 18:02:26 +0300

    added delta message to chatbot

--- strategies/consumers.py
+++ strategies/consumers.py
@@ -43,18 +43,52 @@ class StrategyConsumer(AsyncWebsocketConsumer):
             )
 
     async def chat_message(self, event):
-        message = event['message'].upper()
-        if message == 'RUN XBTUSD':
+        resolved_query = event['message']
+        command = resolved_query.upper()
+        response = self.prepare_response(resolved_query)
+
+        if command == 'RUN XBTUSD':
             self.manager.place_orders('XBTUSD')
+            response['result']['fulfillment']['messages'].append(
+                self.prepare_message('Opening orders XBTUSD')
+            )
 
-        elif message == 'RUN XBTM18':
+        elif command == 'RUN XBTM18':
             self.manager.place_orders('XBTM18')
+            response['result']['fulfillment']['messages'].append(
+                self.prepare_message('Opening orders XBTM18')
+            )
 
-        elif 'RUN DELTA' in message.lower():
+        elif command == 'DELTA':
             delta = self.manager.exchange.calc_pts_delta()
-            message = delta
+            text = 'Delta: %.4f' % delta.get('basis')
+            title = 'XBTUSD: %s' % str(delta.get('XBTUSD'))
+            subtitle = 'XBTM18: %s' % str(delta.get('XBTM18'))
+            response['result']['fulfillment']['messages'].append(
+                self.prepare_message(text, title, subtitle)
+            )
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': response
         }))
+
+    def prepare_response(self, query):
+        return {
+            'result': {
+                'resolvedQuery': query,
+                'fulfillment': {
+                    'messages': []
+                }
+            }
+        }
+
+    def prepare_message(self, text, title=None, subtitle=None):
+        return {
+            'type': 'basic_card',
+            'image': None,
+            'buttons': [],
+            'title': title,
+            'subtitle': subtitle,
+            'formattedText': text,
+        }
